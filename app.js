487b1f66306149ebcd64ee733d8461130e66bbe7
Lia Forgione <liaforg@gmail.com>
2017-09-05 16:04:01 -0400

    form test3

---
+++ app.js
@@ -0,0 +1,18 @@
+$(function() {
+    // Get the form.
+    var form = $('#ajax-contact');
+
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+
+    // TODO: The rest of the code will go here...
+});
+
+
+// Set up an event listener for the contact form.
+$(form).submit(function(event) {
+    // Stop the browser from submitting the form.
+    event.preventDefault();
+
+    // TODO
+});
